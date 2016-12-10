e5215f541241ce4c30624952ef4411be888149e2
Clacson <cookiellacson@gmail.com>
2016-12-10 09:53:40 +0000

    make request started

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 var app = function(){
 
-}
+  var url = 'https://api.spotify.com/v1/search?q=christmas&type=album';
+  makeRequest(url, requestComplete);
+
+};
 
 window.onload = app;
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+};
